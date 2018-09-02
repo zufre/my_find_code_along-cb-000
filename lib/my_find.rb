94cbeefca831a8f128c6b1a60f672d32f6e2d8c3
Cedric David <cedric.david@btopenworld.com>
2018-09-02 13:19:44 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
-def my_find(collection)
+def my_find(array)
   i = 0
-  while i < collection.length
-    return collection[i] if yield(collection[i])
+  while i < array.length do
+    if yield(array[i])
+      return array[i]
+    end
     i = i + 1
   end
 end
